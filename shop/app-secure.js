a6225907f32c57744dcaff1bc84acf3acf9f0db1
joroandriamanirisoa13-web <joroandriamanirisoa13@gmail.com>
2025-10-22 14:20:47 +0300

    Update app-secure.js

--- shop/app-secure.js
+++ shop/app-secure.js
@@ -325,14 +325,4 @@ initAuth().then(render);
   render();
 });
 
-  // esorina filtre rehetra
-  filter = 'all';
-  q = '';
-  // UI
-  els.chips.forEach(c => c.classList.remove('active'));
-  const allChip = els.chips.find(c => (c.dataset.filter||'') === 'all');
-  allChip?.classList.add('active');
-  if (els.search) els.search.value = '';
-  render();
-});
 
