81b9f3b90bd898a626e0ff00eef9f1b7f6b46cd5
joroandriamanirisoa13-web <joroandriamanirisoa13@gmail.com>
2025-10-22 16:27:18 +0300

    Update app-secure.js

--- shop/app-secure.js
+++ shop/app-secure.js
@@ -2,7 +2,7 @@ window.addEventListener('error', (e) => { console.error('JS error:', e?.message 
 
 // ========= CONFIG =========
 const SUPABASE_URL = 'https://zogohkfzplcuonkkfoov.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvZ29oa2Z6cGxjdW9ua2tmb292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4Nzk0ODAsImV4cCI6MjA3NjQ1NTQ4MH0.AeQ5pbrwjCAOsh8xxxxxxxxxxxxxxxxxxx';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvZ29oa2Z6cGxjdW9ua2tmb292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4Nzk0ODAsImV4cCI6MjA3NjQ1NTQ4MH0.AeQ5pbrwjCAOsh8DA7pl33B7hLWfaiYwGa36CaeXCsw';
 const OWNER_EMAIL = 'joroandriamanirisoa13@gmail.com';
 
 const BUCKET_MEDIA = 'Media';
